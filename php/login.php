eb2dd4cdf6abf761de0c04c3b394a3d77575d105
jittaphanu <s6404062663037@email.kmutnb.ac.th>
2023-10-17 01:19:46 +0700

    main.php art17/10

--- php/login.php
+++ php/login.php
@@ -16,7 +16,7 @@
         if(mysqli_num_rows($select) > 0){//ในกรณีที่มีบัญชีเเล้ว
             $row = mysqli_fetch_assoc($select);
             $_SESSION['user_id'] = $row['member_id'];
-            header('location: ../html/main.html');
+            header('location: ../php/main.php');
         }else{
             $message[] = 'incorrect email or password';
         }
